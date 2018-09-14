12b150b62e9261235c28b9e7a2ecaaa0c7229d48
deviation <fun.deviation@gmail.com>
2018-09-14 14:49:10 +0900

    #39 회원가입 Activity추가 - 사용자 테이블 컬럼추가 (user_id, user_image)

--- app/src/main/java/net/springcome/winlotto/utils/DatabaseContract.java
+++ app/src/main/java/net/springcome/winlotto/utils/DatabaseContract.java
@@ -2,6 +2,8 @@ package net.springcome.winlotto.utils;
 
 import android.provider.BaseColumns;
 
+import java.sql.Blob;
+
 public final class DatabaseContract {
 
     public static final String DATABASE_NAME = "lotto.db";
@@ -93,20 +95,24 @@ public final class DatabaseContract {
     public static final class User implements  BaseColumns {
         public static final String TABLE_NAME = "user";
         public static final String _ID = BaseColumns._ID;
+        public static final String USER_ID = "user_id";
         public static final String USER_EMAIL = "user_email";
         public static final String USER_PWD = "user_pwd";
         public static final String USER_GRAD = "user_grad";
+        public static final String USER_IMAGE = "user_image";
         public static final String USER_JOIN_DATE = "user_join_date";
 
         public static final String [] COLUMNS = {
-                USER_EMAIL, USER_PWD, USER_GRAD, USER_JOIN_DATE
+                USER_ID, USER_EMAIL, USER_PWD, USER_GRAD, USER_JOIN_DATE, USER_JOIN_DATE
         };
 
         public static final String CREATE_TABLE_SQL = "CREATE TABLE " + TABLE_NAME + " ("
                 + _ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + USER_ID + " TEXT NOT NULL, "
                 + USER_EMAIL + " TEXT NOT NULL, "
                 + USER_PWD + " TEXT NOT NULL, "
                 + USER_GRAD + " TEXT NOT NULL, "
+                + USER_IMAGE + " BLOB NULL, "
                 + USER_JOIN_DATE + " TEXT NOT NULL)";
         public static final String DROP_TABLE_SQL = "DROP TABLE IF EXISTS " + TABLE_NAME;
     }
